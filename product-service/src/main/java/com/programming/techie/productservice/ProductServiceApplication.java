e294108bb75f31212dd7e830fe8a09d8390d880b
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-30 15:12:12 -0300

    :tada: create product-service MS

---
+++ product-service/src/main/java/com/programming/techie/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProductServiceApplication.class, args);
+  }
+
+}
