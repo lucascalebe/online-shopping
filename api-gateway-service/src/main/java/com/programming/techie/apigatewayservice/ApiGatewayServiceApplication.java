af83e35ca74e7fb9535c5f9707fa28500af35e04
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-30 21:48:06 -0300

    :tada: create api-gateway MS

---
+++ api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
