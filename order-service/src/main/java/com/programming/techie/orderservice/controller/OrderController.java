1e155b01edf056bdf611697b7579a392ccd52143
Lucas Calebe <lucascalebe07@hotmail.com>
2022-12-11 13:31:43 -0300

    :sparkles: add circuit breaker to endpoint

--- order-service/src/main/java/com/programming/techie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programming/techie/orderservice/controller/OrderController.java
@@ -6,12 +6,15 @@ import com.programming.techie.orderservice.model.Order;
 import com.programming.techie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @Slf4j
 @RestController
@@ -21,11 +24,15 @@ public class OrderController {
 
   private final OrderRepository orderRepository;
   private final InventoryClient inventoryClient;
+  private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
   @PostMapping
   public String placeOrder(@RequestBody OrderDto orderDto) {
-    boolean allProductsInStock = orderDto.getOrderLineItems().stream()
+    Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("invenotry");
+    Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItems().stream()
             .allMatch(orderLineItem -> inventoryClient.isInStock(orderLineItem.getSkuCode()));
+
+    boolean allProductsInStock = circuitBreaker.run(booleanSupplier, throwable -> handleErrorCase());
 
     if (allProductsInStock) {
       Order order = new Order();
@@ -38,5 +45,9 @@ public class OrderController {
     }
 
     return "Order failed, one of the products in the order is not in stock";
+  }
+
+  private Boolean handleErrorCase() {
+    return false;
   }
 }
